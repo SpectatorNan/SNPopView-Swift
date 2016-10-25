ec5474d4bfce621ce7a3d51b1894ecaaf74fa4d2
spectatorNan <spectatornan@gmail.com>
2016-10-26 01:23:45 +0800

    优化 fix BUG

--- SNPopView-Swift/SNPopView.swift
+++ SNPopView-Swift/SNPopView.swift
@@ -9,36 +9,49 @@
 import UIKit
 
 class SNPopView: UIView {
+    var title: String?
+//   lazy var contentArr: Array<Any> = { [unowned self] in
+//    let result = self.dataSource?.snpopView(models: self)
+//    return result!
+//    }()
     
-    var contentArr: Array<Any> {
-        
-        return (self.dataSource?.snpopView(models: self))!
-    }
+    var contentArr: Array<Any>?
+    
     
     var delegate: SNPopViewDelegate?
     var dataSource: SNPopViewDataSource?
-    
+    fileprivate var top: UIView?
+//    fileprivate var mid: UIView?
+    fileprivate var bottom: UIView?
 
  
     
     convenience init() {
     self.init(frame: UIScreen.main.bounds)
-    
-    self.backgroundColor = UIColor(white: 0, alpha: 0.3)
+        self.alpha = 0
+        self.backgroundColor = UIColor(white: 0, alpha: 0.3)
     }
     
-   
+    func reloadContent() {
+        table?.reloadData()
+    }
     
-    
+    fileprivate var table: UITableView?
 }
 
 
 extension SNPopView {
     
-     func popAlert(to:UIView) {
-        
-//        let superView =  SNPopView(frame: UIScreen.main.bounds)
+    
+    
+    func popAlert(to:UIView, title: String?) {
+        self.title = title
+        self.contentArr = self.dataSource?.snpopView(models: self)
         to.addSubview(self)
+        
+        UIView.animate(withDuration: 0.4) { 
+            self.alpha = 1
+        }
         
         let width = snPopScreenWidth-40
         let height = snPopScreenHeight*0.3
@@ -46,10 +59,13 @@ extension SNPopView {
         let contentView = UITableView(frame: setFrameByCenter(width: width, height: height))
         contentView.dataSource = self
         contentView.delegate = self
+        contentView.alpha = 1
         self.addSubview(contentView)
+        table = contentView
         
         if let headerView = self.delegate?.snpopView?(forHeader: self) {
             headerView.frame = setFrameOnTopOf(view: contentView, height: headerView.frame.height)
+            self.top = headerView
             self.addSubview(headerView)
         } else {
           //MARK:  顶部圆角
@@ -57,6 +73,7 @@ extension SNPopView {
         
         if let footerView = self.delegate?.snpopView?(forFooter: self) {
             footerView.frame = setFrameOnBottomOf(view: contentView, height: footerView.frame.height)
+            self.bottom = footerView
             self.addSubview(footerView)
         } else {
             //MARK: 底部圆角
@@ -64,13 +81,18 @@ extension SNPopView {
     }
     
      func dismiss() {
-        
-        UIView.animate(withDuration: 0.8, animations: {
+
+        UIView.animate(withDuration: 0.8, animations: { 
             self.alpha = 0
-        }) { (valid) in
-            if valid {
-                self.removeFromSuperview()
-            }
+            }) { (valid) in
+                if valid {
+                    self.table = nil
+                    self.top = nil
+                    self.bottom = nil
+//                    self.contentArr = nil
+                    self.removeFromSuperview()
+//                    self.alpha = 1
+                }
         }
     }
     
@@ -80,7 +102,7 @@ extension SNPopView {
 extension SNPopView: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.contentArr.count
+        return self.contentArr!.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -93,7 +115,8 @@ extension SNPopView: UITableViewDataSource {
 extension SNPopView: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.delegate?.snpopView?(popView: self,didSelcted: contentArr[indexPath.row])
+//        self.delegate?.snpopView?(popView: self,didSelcted: contentArr[indexPath.row])
+        self.delegate?.snpopView?(popView: self, didSelcted: tableView.cellForRow(at: indexPath)!, model: contentArr?[indexPath.row])
     }
     
 }
@@ -130,7 +153,7 @@ func setFrameOnBottomOf(view:UIView, height: CGFloat) -> CGRect {
     
    @objc optional func snpopView(forFooter popView: SNPopView) -> UIView?
    @objc optional func snpopView(forHeader popView: SNPopView) -> UIView?
-    @objc optional func snpopView(popView: SNPopView, didSelcted rowModel: Any)
+    @objc optional func snpopView(popView: SNPopView, didSelcted row:UITableViewCell,model rowModel: Any)
  }
 
 protocol SNPopViewDataSource {
