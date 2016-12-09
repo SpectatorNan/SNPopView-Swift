804e3f558a9d84f45762699f31d11446288d0a15
spectatorNan <spectatornan@gmail.com>
2016-12-10 00:17:40 +0800

    圆角处理

--- SNPopView-Swift/SNPopView.swift
+++ SNPopView-Swift/SNPopView.swift
@@ -23,9 +23,9 @@ import Foundation
     var delegate: SNPopViewDelegate?
     var dataSource: SNPopViewDataSource?
     var frameDelegate: SNPopViewFrameDelegate?
-    fileprivate var top: UIView?
+     var top: UIView?
 //    fileprivate var mid: UIView?
-    fileprivate var bottom: UIView?
+     var bottom: UIView?
 
  
     
@@ -47,9 +47,10 @@ extension SNPopView {
     
     
     
-    func popAlert(to:UIView, title: String?) {
+    func popAlert(to:UIView, title: String?, content: Array<Any>) {
         self.title = title
-        self.contentArr = self.dataSource?.snpopView(models: self)
+//        self.contentArr = self.dataSource?.snpopView(models: self)
+        self.contentArr = content
         to.addSubview(self)
         
         UIView.animate(withDuration: 0.4) { 
@@ -59,11 +60,15 @@ extension SNPopView {
         let width = snPopScreenWidth-40
         let height = snPopScreenHeight*0.3
         
-        let contentView = UITableView(frame: setFrameByCenter(width: width, height: height))
+        let contentView = UITableView()
+
         
-//        if let size = self.delegate?.snpopView?(forSize: self) {
-//            contentView.frame.size = size
-//        }
+         contentView.frame =  CGRect(x: 0, y: 0, width: width, height: height)
+        
+        let contentSuperView = UIView()
+        contentSuperView.frame = adjustCenter(width: width, height: height)
+        
+        contentSuperView.addSubview(contentView)
         
         if let size = self.frameDelegate?.snpopViewSize(popView: self) {
 //            contentView.frame.size = size
@@ -73,26 +78,38 @@ extension SNPopView {
         contentView.dataSource = self
         contentView.delegate = self
         contentView.alpha = 1
-        self.addSubview(contentView)
+        contentView.backgroundColor = .white
+        self.addSubview(contentSuperView)
         table = contentView
         
         if let headerView = self.delegate?.snpopView?(forHeader: self) {
-            headerView.frame = setFrameOnTopOf(view: contentView, height: headerView.frame.height)
+            headerView.frame = setFrameOnTopOf(view: contentSuperView, height: headerView.frame.height)
             self.top = headerView
+            
+//            self.layoutIfNeeded()
+            headerView.cornerByRoundingCorners(corners: [.topLeft, .topRight], cornerRadii: CGSize(width: 8, height: 8))
+            
             self.addSubview(headerView)
         } else {
           //MARK:  顶部圆角
-            
+            contentSuperView.cornerByRoundingCorners(corners: [.topLeft, .topRight], cornerRadii: CGSize(width: 8, height: 8))
             
         }
         
         if let footerView = self.delegate?.snpopView?(forFooter: self) {
-            footerView.frame = setFrameOnBottomOf(view: contentView, height: footerView.frame.height)
+            footerView.frame = setFrameOnBottomOf(view: contentSuperView, height: footerView.frame.height)
             self.bottom = footerView
+            
+            footerView.cornerByRoundingCorners(corners: [.bottomLeft, .bottomRight], cornerRadii: CGSize(width: 8, height: 8))
+            
             self.addSubview(footerView)
         } else {
             //MARK: 底部圆角
-            
+            if let _ = self.top {
+                contentSuperView.cornerByRoundingCorners(corners: [.bottomLeft, .bottomRight], cornerRadii: CGSize(width: 8, height: 8))
+            } else {
+                contentSuperView.cornerByRoundingCorners(corners: .allCorners, cornerRadii: CGSize(width: 8, height: 8))
+            }
         }
     }
     
@@ -103,13 +120,17 @@ extension SNPopView {
             self.alpha = 0
             }) { (valid) in
                 if valid {
-                    self.top?.removeFromSuperview()
-                    self.bottom?.removeFromSuperview()
+//                    self.top?.removeFromSuperview()
+//                    self.bottom?.removeFromSuperview()
+                    
+                    for subV in self.subviews {
+                        subV.removeFromSuperview()
+                    }
                     
                     self.table = nil
                     self.top = nil
                     self.bottom = nil
-//                    self.contentArr = nil
+                    self.contentArr = nil
                     self.removeFromSuperview()
 //                    self.alpha = 1
                     
@@ -145,10 +166,26 @@ extension SNPopView: UITableViewDelegate {
 let snPopScreenHeight = UIScreen.main.bounds.size.height
 let snPopScreenWidth = UIScreen.main.bounds.size.width
 
+extension UIView {
+    
+    func adjustCenter(width: CGFloat, height: CGFloat) -> CGRect {
+        
+        let centerX = self.superview?.center.x
+        let centerY = self.superview?.center.y
+        
+        let x = centerX! - width/2
+        let y = centerY! - height/2
+        
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+}
+
+
 func setFrameByCenter(width:CGFloat, height:CGFloat) -> CGRect {
     
     let x = snPopScreenWidth/2 - (width)/2
     let y = snPopScreenHeight/2 - (height)/2
+    
     
     return CGRect(x: x, y: y, width: width, height: height)
 }
@@ -183,5 +220,15 @@ protocol SNPopViewFrameDelegate {
 
 protocol SNPopViewDataSource: NSObjectProtocol {
     func snpopView(contentView popView: SNPopView,tableView table: UITableView,  cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    func snpopView(models popView: SNPopView) -> Array<Any>
+//    func snpopView(models popView: SNPopView) -> Array<Any>
+}
+
+extension UIView {
+    func cornerByRoundingCorners(corners: UIRectCorner, cornerRadii: CGSize) {
+    let maskPath = UIBezierPath.init(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: cornerRadii)
+    let maskLayer = CAShapeLayer()
+    maskLayer.frame = self.bounds
+    maskLayer.path = maskPath.cgPath
+    self.layer.mask = maskLayer
+}
 }
